fff80df8a9e64ceabc4e92cea70b3da4b4a63804
Bruno Becoski <brunobecoski@gmail.com>
2023-06-14 22:15:49 -0300

    Criando o arquivo books.ts

---
+++ prisma/constants/books.ts
@@ -0,0 +1,288 @@
+export const books = [
+  {
+    id: 'c8176d86-896a-4c21-9219-6bb28cccaa5f',
+    name: '14 Hábitos de Desenvolvedores Altamente Produtivos',
+    author: 'Zeno Rocha',
+    summary:
+      'Você pode aprender os framework mais populares, usar as melhores linguagens de programação e trabalhar nas maiores empresas de tecnologia, mas se você cultivar maus hábitos, vai ser difícil se tornar um desenvolvedor de ponta.',
+    cover:
+      '/images/books/14-habitos-de-desenvolvedores-altamente-produtivos.png',
+    total_pages: 135,
+    categories: [
+      {
+        name: 'Educação',
+        id: 'f1a50507-0aa7-4245-8a5c-0d0de14e9d6d',
+      },
+      {
+        name: 'Programação',
+        id: 'c9f22067-4978-4a24-84a1-7d37f343dfc2',
+      },
+    ],
+  },
+  {
+    id: '375948a7-bca3-4b59-9f97-bfcde036b4ca',
+    name: 'O Hobbit',
+    author: 'J.R.R Tolkien',
+    summary:
+      'Bilbo Bolseiro era um dos mais respeitáveis hobbits de todo o Condado até que, um dia, o mago Gandalf bate à sua porta. A partir de então, toda sua vida pacata e campestre soprando anéis de fumaça com seu belo cachimbo começa a mudar. Ele é convocado a participar de uma aventura por ninguém menos do que Thorin Escudo-de-Carvalho, um príncipe do poderoso povo dos Anãos.',
+    cover_url: '/images/books/o-hobbit.png',
+    total_pages: 385,
+    categories: [
+      {
+        name: 'Ficção',
+        id: '8c4a4a4d-cbc4-4d2c-bb46-e95b0a536e09',
+      },
+      {
+        name: 'Aventura',
+        id: 'e9c6d3f6-f3ec-4c52-ae28-6adcbab6ee67',
+      },
+    ],
+  },
+  {
+    id: '86596503-369b-4614-bacf-11c9bb73e779',
+    name: 'O guia do mochileiro das galáxias',
+    author: 'Douglas Adams',
+    summary:
+      'Conta as aventuras espaciais do inglês Arthur Dent e de seu amigo Ford Prefect. A dupla escapa da destruição da Terra pegando carona numa nave alienígena, graças aos conhecimentos de Prefect, um E.T. que vivia disfarçado de ator desempregado enquanto fazia pesquisa de campo para a nova edição do Guia do Mochileiro das Galáxias, o melhor guia de viagens interplanetário.',
+    cover: '/images/books/o-guia-do-mochileiro-das-galáxias.png',
+    total_pages: 241,
+    categories: [
+      {
+        name: 'Ficção',
+        id: '8c4a4a4d-cbc4-4d2c-bb46-e95b0a536e09',
+      },
+      {
+        name: 'Geek',
+        id: '2e65c193-325a-40c3-98f3-6c13e9b75b02',
+      },
+    ],
+  },
+  {
+    id: 'd0d70b05-d48f-4d83-b1e8-0b4dd984c97d',
+    name: 'A revolução dos bichos',
+    author: 'George Orwell',
+    summary:
+      'A revolução dos bichos é uma fábula sobre o poder. Narra a insurreição dos animais de uma granja contra seus donos. Progressivamente, porém, a revolução degenera numa tirania ainda mais opressiva que a dos humanos.',
+    cover_url: '/images/books/a-revolucao-dos-bixos.png',
+    total_pages: 157,
+    categories: [
+      {
+        name: 'Alegoria',
+        id: 'a1d0ee25-9c9a-49c8-84eb-7af1e0dd356d',
+      },
+      {
+        name: 'Fábula',
+        id: '997f8a10-21fb-4c80-bd16-17e8b79a31a3',
+      },
+    ],
+  },
+  {
+    id: '48b86ac2-014e-401d-bcbb-331ce5f4a457',
+    name: 'O fim da eternidade',
+    author: 'Isaac Asimov',
+    summary:
+      'Andrew Harlan é um Eterno: membro da classe dominante do futuro. Seu trabalho é viajar pelos séculos monitorando e alterando realidades, corrigindo assim os erros dos homens. A humanidade estava a salvo. Até que Harlan comete o pior dos pecados: apaixona-se.',
+    cover_url: '/images/books/o-fim-da-eternidade.png',
+    total_pages: 272,
+    categories: [
+      {
+        name: 'Ficção',
+        id: '8c4a4a4d-cbc4-4d2c-bb46-e95b0a536e09',
+      },
+      {
+        name: 'Romance',
+        id: '70efc33d-7d6b-4db4-bab6-524c4c4b2e2c',
+      },
+    ],
+  },
+  {
+    id: 'e688c24f-d14d-4607-a12e-90e6e367398d',
+    name: 'Entendendo Algoritmos',
+    author: 'Aditya Y. Bhargava',
+    summary:
+      'Um algoritmo nada mais é do que um procedimento passo a passo para a resolução de um problema. Os algoritmos que você mais utilizará como um programador já foram descobertos, testados e provados.',
+    cover_url: '/images/books/entendo-algoritmos',
+    total_pages: 400,
+    categories: [
+      {
+        name: 'Programação',
+        id: 'c9f22067-4978-4a24-84a1-7d37f343dfc2',
+      },
+      {
+        name: 'Educação',
+        id: 'f1a50507-0aa7-4245-8a5c-0d0de14e9d6d',
+      },
+    ],
+  },
+  {
+    id: '0440ad7d-230e-4573-b455-84ca38b5d339',
+    name: 'Código Limpo',
+    author: 'Robert C. Martin',
+    summary:
+      'Mesmo um código ruim pode funcionar. Mas se ele não for limpo, pode acabar com uma empresa de desenvolvimento. Perdem-se a cada ano horas incontáveis e recursos importantes devido a um código mal escrito. Mas não precisa ser assim.',
+    cover_url: '/images/books/codigo-limpo.png',
+    total_pages: 425,
+    categories: [
+      {
+        name: 'Programação',
+        id: 'c9f22067-4978-4a24-84a1-7d37f343dfc2',
+      },
+      {
+        name: 'Educação',
+        id: 'f1a50507-0aa7-4245-8a5c-0d0de14e9d6d',
+      },
+    ],
+  },
+  {
+    id: '14f410df-b28a-4e72-b1b4-363e26e160dd',
+    name: 'O poder do hábito',
+    author: 'Charles Duhigg',
+    summary:
+      'Durante os últimos dois anos, uma jovem transformou quase todos os aspectos de sua vida. Parou de fumar, correu uma maratona e foi promovida. Em um laboratório, neurologistas descobriram que os padrões dentro do cérebro dela mudaram de maneira fundamental.',
+    cover_url: '/images/books/o-poder-do-habito.png',
+    total_pages: 408,
+    categories: [
+      {
+        name: 'Autoajuda',
+        id: 'a4d63d4e-f8ad-4a60-b7b9-9d925a2a8a92',
+      },
+      {
+        name: 'Educação',
+        id: 'f1a50507-0aa7-4245-8a5c-0d0de14e9d6d',
+      },
+    ],
+  },
+  {
+    id: 'd2870ad0-3312-4ac2-af9f-76af6565587d',
+    name: 'Arquitetura limpa',
+    author: 'Robert C. Martin',
+    summary:
+      'As regras universais de arquitetura de software aumentam dramaticamente a produtividade dos desenvolvedores ao longo da vida dos sistemas de software.',
+    cover_url: '/images/books/arquitetura-limpa.png',
+    total_pages: 432,
+    categories: [
+      {
+        name: 'Programação',
+        id: 'c9f22067-4978-4a24-84a1-7d37f343dfc2',
+      },
+      {
+        name: 'Educação',
+        id: 'f1a50507-0aa7-4245-8a5c-0d0de14e9d6d',
+      },
+    ],
+  },
+  {
+    id: '4fd2b389-b211-40b5-9797-f78cbb985645',
+    name: 'Histórias extraordinárias',
+    author: 'Edgar Allan Poe',
+    summary:
+      'Histórias extraordinárias reúne dezoito contos assombrosos. O caráter macabro das histórias, dotadas de profundidade psicológica e imersas em uma atmosfera eletrizante.',
+    cover_url: '/images/books/historias-extraordinarias.png',
+    total_pages: 448,
+    categories: [
+      {
+        name: 'Ficção',
+        id: '8c4a4a4d-cbc4-4d2c-bb46-e95b0a536e09',
+      },
+      {
+        name: 'Suspense',
+        id: '7c8dc74a-2e03-4d72-96de-822e332e5530',
+      },
+    ],
+  },
+  {
+    id: '6de9f6b8-5ff4-4e06-b9f4-843eca462803',
+    name: 'Refatoração',
+    author: 'Martin Fowler',
+    summary:
+      'O renomado mentor da tecnologia de objetos Martin Fowler demonstra que, com treinamento apropriado, um desenhista de sistemas experiente pode pegar um projeto ruim e retrabalhá-lo num código robusto e bem projetado.',
+    cover_url: '/images/books/refatoracao.png',
+    total_pages: 440,
+    categories: [
+      {
+        name: 'Programação',
+        id: 'c9f22067-4978-4a24-84a1-7d37f343dfc2',
+      },
+      {
+        name: 'Educação',
+        id: 'f1a50507-0aa7-4245-8a5c-0d0de14e9d6d',
+      },
+    ],
+  },
+  {
+    id: 'd0590f9a-dd89-42fd-9bbb-bf26c2e4dcf9',
+    name: 'Domain-Drivem Design',
+    author: 'Eric Evans',
+    summary:
+      'A comunidade de desenvolvimento de softwares reconhece que a modelagem de domínios é fundamental para o design de softwares. Através de modelos de domínios, os desenvolvedores de software conseguem expressar valiosas funcionalidades e traduzi-las em uma implementação de software que realmente atenda às necessidades de seus usuários.',
+    cover_url: '/images/books/domain-driven-design.png',
+    total_pages: 528,
+    categories: [
+      {
+        name: 'Programação',
+        id: 'c9f22067-4978-4a24-84a1-7d37f343dfc2',
+      },
+      {
+        name: 'Arquitetura',
+        id: 'a2891eaa-6d9e-48d8-a86a-10aa017d3d3f',
+      },
+    ],
+  },
+  {
+    id: '1d5cdbdc-b90f-40d5-8fe9-d4923ae12dbd',
+    name: 'Viagem ao Centro da Terra',
+    author: 'Julio Verne',
+    summary:
+      'O cientista alemão Lidenbrock encontra um pergaminho criptografado dentro de um livro antigo. Ele decifra a mensagem e descobre que um alquimista do século XVI viajou ao centro da Terra. Acompanhado do sobrinho Axel, vai à Islândia para repetir a façanha.',
+    cover: '/images/books/viagem-ao-centro-da-terra.png',
+    total_pages: 304,
+    categories: [
+      {
+        name: 'Romance',
+        id: '70efc33d-7d6b-4db4-bab6-524c4c4b2e2c',
+      },
+      {
+        name: 'Ficção',
+        id: '8c4a4a4d-cbc4-4d2c-bb46-e95b0a536e09',
+      },
+    ],
+  },
+  {
+    id: '404e47f8-da53-44fd-ab53-37ed171c3a9f',
+    name: 'Fragmentos do Horror',
+    author: 'Junji Ito',
+    summary:
+      'Fragmentos do Horror é uma coleção de histórias curtas, perfeitas para quem quer experimentar o que essa mente tão delirante é capaz de produzir.',
+    cover_url: '/images/books/fragmentos-do-horror.png',
+    total_pages: 224,
+    categoires: [
+      {
+        name: 'Ficção',
+        id: '8c4a4a4d-cbc4-4d2c-bb46-e95b0a536e09',
+      },
+      {
+        name: 'Terror',
+        id: 'a0a61b53-37d7-48ec-9b92-6db074f6d9c9',
+      },
+    ],
+  },
+  {
+    id: '66cb0f47-7e20-4492-b640-9c020fcae6f2',
+    name: 'O Programador Pragmático',
+    author: 'Andrew Hunt',
+    summary:
+      'O Programador Pragmático ilustra as melhores práticas e as principais armadilhas do desenvolvimento de software. Destinado a todos envolvidos com programação, de codificadores iniciantes a programadores experientes e gerentes responsáveis por projetos de software, apresenta lições simples que promovem rápidas melhorias na produtividade pessoal, precisão e satisfação profissional.',
+    cover_url: '/images/books/o-programador-pragmatico.png',
+    total_pages: 344,
+    categories: [
+      {
+        name: 'Programação',
+        id: 'c9f22067-4978-4a24-84a1-7d37f343dfc2',
+      },
+      {
+        name: 'Educação',
+        id: 'f1a50507-0aa7-4245-8a5c-0d0de14e9d6d',
+      },
+    ],
+  },
+]
