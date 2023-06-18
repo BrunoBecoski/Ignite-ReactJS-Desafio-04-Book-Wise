9e53c34ca7ab4d4cb7f13da429c0dc80350f1a02
Bruno Becoski <brunobecoski@gmail.com>
2023-06-18 10:49:56 -0300

    Atualizando o arquivo ratings.ts

--- prisma/constants/ratings.ts
+++ prisma/constants/ratings.ts
@@ -1,114 +1,130 @@
 export const ratings = [
   {
-    id: 'b3cf5553-5883-48c6-b89b-9df143217a49',
-    rate: 4,
-    description: '',
-    book_id: '375948a7-bca3-4b59-9f97-bfcde036b4ca', // O Hobbit
-    user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60', // Jaxson Dias
-  },
-  {
-    id: '4d91e131-d31e-41af-985d-0ca3c52943e9',
-    rate: 4,
-    description: '',
-    book_id: '86596503-369b-4614-bacf-11c9bb73e779', // O Guia do Mochileiro das Galáxias
-    user_id: 'c296c6c0-5c59-40dd-aa8a-ef2b015b7502', // Brandom Botosh
-  },
-  {
-    id: 'acab57d3-52e0-4bc8-980c-11aa8d917158',
-    rate: 5,
-    description: '',
-    book_id: 'e688c24f-d14d-4607-a12e-90e6e367398d', // Entendendo Algoritmos
-    user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef', // Lindsey Philips
-  },
-  {
-    id: '23ccd33a-bcdb-4858-bc17-0d61b02320a9',
-    rate: 4,
-    description: '',
-    book_id: '48b86ac2-014e-401d-bcbb-331ce5f4a457', // O Fim da Eternidade
-    user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60', // Jaxson Dias
-  },
-  {
     id: '20a90e99-b933-4fe1-8e4b-f2f6682f47d7',
     rate: 4,
-    description: '',
+    description:
+      'Uma narrativa construída em uma alegoria, através da utilização da retórica, podemos ver os animais de certa forma representando o ser humano e também representando os dois frontes geo-politicos da época em que Orwell estava visualizando o mundo.',
     book_id: 'd0d70b05-d48f-4d83-b1e8-0b4dd984c97d', // A Revolução dos Bichos
-    user_id: 'c296c6c0-5c59-40dd-aa8a-ef2b015b7502', // Brandom Botosh
-  },
-  {
-    id: 'bfba092f-ebdd-485f-8385-bbb3344e31e8',
-    rate: 5,
-    description: '',
-    book_id: '375948a7-bca3-4b59-9f97-bfcde036b4ca', // O Hobbit
-    user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef', // Lindsey Philips
-  },
-  {
-    id: 'a6ef3fd0-54d2-4bda-884d-1a31499f7baf',
-    rate: 4,
-    description: '',
-    book_id: 'c8176d86-896a-4c21-9219-6bb28cccaa5f', // 14 Hábitos de Desenvolvedores Altamente Produtivos
-    user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60', // Jaxson Dias
+    user_id: 'c296c6c0-5c59-40dd-aa8a-ef2b015b7502', // Brandon Botosh
   },
   {
     id: 'abe3096f-5ea5-4a91-81cb-e9d279fc9312',
-    rate: 4,
-    description: '',
-    book_id: '0440ad7d-230e-4573-b455-84ca38b5d339', // Código Limpo
-    user_id: 'c296c6c0-5c59-40dd-aa8a-ef2b015b7502', // Brandom Botosh
-  },
-  {
-    id: '5f8d1385-4139-45b5-9c90-8d47845f64c9',
     rate: 5,
-    description: '',
-    book_id: '14f410df-b28a-4e72-b1b4-363e26e160dd', // O Poder do Hábito
-    user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef', // Lindsey Philips
-  },
-  {
-    id: 'fc1ce6d7-b591-4fba-a6f3-7de81c9666c8',
-    rate: 4,
-    description: '',
-    book_id: 'd2870ad0-3312-4ac2-af9f-76af6565587d', // Arquitetura Limpa
-    user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60', // Jaxson Dias
+    description:
+      'O livro aborda práticas e princípios para escrever um código de qualidade, legível, sustentável e fácil de manter. O texto enfatiza a importância de escrever código que seja compreensível para os outros desenvolvedores que irão trabalhar nele.',
+    book_id: '0440ad7d-230e-4573-b455-84ca38b5d339', // Código Limpo
+    user_id: 'c296c6c0-5c59-40dd-aa8a-ef2b015b7502', // Brandon Botosh
   },
   {
     id: '7df46c81-e5e2-43e5-8208-6b42a18d910d',
     rate: 4,
-    description: '',
+    description:
+      'Quem gosta de terror, gosta de Stephen King e companhia esse livro é ótimo. Sou fã das obras de Edgar Alan Poe mas é uma escrita fluida, bem sólida o que ajuda na experiência do leitor.',
     book_id: '4fd2b389-b211-40b5-9797-f78cbb985645', // Histórias Extraordinárias
-    user_id: 'c296c6c0-5c59-40dd-aa8a-ef2b015b7502', // Brandom Botosh
+    user_id: 'c296c6c0-5c59-40dd-aa8a-ef2b015b7502', // Brandon Botosh
   },
   {
-    id: '7058f255-0b3b-47d7-a68a-1dc8915fda2c',
-    rate: 5,
-    description: '',
-    book_id: '6de9f6b8-5ff4-4e06-b9f4-843eca462803', // Refatoração
-    user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef', // Lindsey Philips
-  },
-  {
-    id: '65398a13-cf3e-40d8-baf5-351bda466169',
-    rate: 4,
-    description: '',
-    book_id: 'd0590f9a-dd89-42fd-9bbb-bf26c2e4dcf9', // Domain-Driven Design
-    user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60', // Jaxson Dias
+    id: '4d91e131-d31e-41af-985d-0ca3c52943e9',
+    rate: 3,
+    description:
+      'É um livro muito bem escrito. Os personagens fazem uma viagem fantástica visitando planetas hipotéticos com descrição com muito humor.',
+    book_id: '86596503-369b-4614-bacf-11c9bb73e779', // O Guia do Mochileiro das Galáxias
+    user_id: 'c296c6c0-5c59-40dd-aa8a-ef2b015b7502', // Brandon Botosh
   },
   {
     id: '0bb3d43e-38e1-4772-a2e5-135aa9e0e843',
     rate: 4,
-    description: '',
+    description:
+      'Um livro extraordinário! O autor não teve barreira na sua imaginação para criar esse livro cheio de aventura e ficção.',
     book_id: '1d5cdbdc-b90f-40d5-8fe9-d4923ae12dbd', // Viagem ao Centro da Terra
-    user_id: 'c296c6c0-5c59-40dd-aa8a-ef2b015b7502', // Brandom Botosh
+    user_id: 'c296c6c0-5c59-40dd-aa8a-ef2b015b7502', // Brandon Botosh
+  },
+  {
+    id: 'a6ef3fd0-54d2-4bda-884d-1a31499f7baf',
+    rate: 5,
+    description:
+      'Livro muito bom, abriu a minha mente para tantas coisas, eu gostei bastante, cada incentivo top para quem é desenvolvedor.',
+    book_id: 'c8176d86-896a-4c21-9219-6bb28cccaa5f', // 14 Hábitos de Desenvolvedores Altamente Produtivos
+    user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60', // Jaxson Dias
+  },
+  {
+    id: 'fc1ce6d7-b591-4fba-a6f3-7de81c9666c8',
+    rate: 4,
+    description:
+      'Arquitetura limpa faz você questionar completamente a forma como sua arquitetura funciona. O livro constantemente te convida a entender quais componentes devem depender do quê e definitivamente mudou a forma como eu enchergo meus sistemas.',
+    book_id: 'd2870ad0-3312-4ac2-af9f-76af6565587d', // Arquitetura Limpa
+    user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60', // Jaxson Dias
+  },
+  {
+    id: '65398a13-cf3e-40d8-baf5-351bda466169',
+    rate: 4,
+    description:
+      'Livro conceitual obrigatório para quem quer construir sistemas grandes com uma arquitetura que permita uma manutenção mais estável em um longo prazo.',
+    book_id: 'd0590f9a-dd89-42fd-9bbb-bf26c2e4dcf9', // Domain-Driven Design
+    user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60', // Jaxson Dias
+  },
+  {
+    id: '23ccd33a-bcdb-4858-bc17-0d61b02320a9',
+    rate: 4,
+    description:
+      'Incrível como a obra continua tocante, independente de quantas vezes ou quando você lê: poucos livros combinam ciência, amor, sacrifício, ética, moral e outros assuntos tão delicados em um único volume, com tamanha precisão.',
+    book_id: '48b86ac2-014e-401d-bcbb-331ce5f4a457', // O Fim da Eternidade
+    user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60', // Jaxson Dias
+  },
+  {
+    id: 'b3cf5553-5883-48c6-b89b-9df143217a49',
+    rate: 4,
+    description:
+      'Nosso estimado “O Hobbit” é leitura obrigatória para todo jovem que deseja ingressar no universo das aventuras, como tambem, porta de entrada da saga de Tolkien. É uma obra cuja leitura é de fácil acesso, fluída, fundamental para formação do imaginário em jovens desta idade.',
+    book_id: '375948a7-bca3-4b59-9f97-bfcde036b4ca', // O Hobbit
+    user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60', // Jaxson Dias
+  },
+  {
+    id: '4d71cb4c-1805-4cb3-a13a-03bd484ce324',
+    rate: 5,
+    description:
+      'Este livro é uma coletânea de abordagens e comportamentos que vão te ajudar a sair da mediocridade e se tornar um excelente programador. Você vai perceber que quase todos, se não todos, os programadores que admira segue ideias apresentadas aqui.',
+    book_id: '66cb0f47-7e20-4492-b640-9c020fcae6f2', // O Programador Pragmático
+    user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60', // Jaxson Dias
+  },
+  {
+    id: 'acab57d3-52e0-4bc8-980c-11aa8d917158',
+    rate: 5,
+    description:
+      'Para adquirir mais conhecimentos e estudo na área de Programação.',
+    book_id: 'e688c24f-d14d-4607-a12e-90e6e367398d', // Entendendo Algoritmos
+    user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef', // Lindsey Philips
   },
   {
     id: 'cdf60495-48e4-4b9e-ae55-50005b06ef0a',
-    rate: 5,
-    description: '',
+    rate: 4,
+    description:
+      'Para quem é fã do autor essa coletânea reúne excelentes histórias. Os traços estão bem detalhados e a qualidade da folha é ótima. Recomendo muito para quem quer começar a conhecer o autor e também para quem já conhece seu trabalho.',
     book_id: '404e47f8-da53-44fd-ab53-37ed171c3a9f', // Fragmentos do Horror
     user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef', // Lindsey Philips
   },
   {
-    id: '4d71cb4c-1805-4cb3-a13a-03bd484ce324',
+    id: 'bfba092f-ebdd-485f-8385-bbb3344e31e8',
     rate: 4,
-    description: '',
-    book_id: '66cb0f47-7e20-4492-b640-9c020fcae6f2', // O Programador Pragmático
-    user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60', // Jaxson Dias
+    description:
+      'Aqui temos uma história repleta de aventuras, criaturas fantásticas. Foi maravilhoso acompanhar Bilbo Bolseiro, Thorin Escudo de Carvalho e companhia, Tolkien te faz sentir parte da história, me fez ficar com medo diante de tais perigos.',
+    book_id: '375948a7-bca3-4b59-9f97-bfcde036b4ca', // O Hobbit
+    user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef', // Lindsey Philips
+  },
+  {
+    id: '5f8d1385-4139-45b5-9c90-8d47845f64c9',
+    rate: 4,
+    description:
+      'Livro nos fez refletir sobre hábitos, comportamentos automatizados e nos dá instrumentos para repensar nossos hábitos e substituir por outros melhores.',
+    book_id: '14f410df-b28a-4e72-b1b4-363e26e160dd', // O Poder do Hábito
+    user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef', // Lindsey Philips
+  },
+  {
+    id: '7058f255-0b3b-47d7-a68a-1dc8915fda2c',
+    rate: 5,
+    description:
+      'O conteúdo é de fácil entendimento e atende completamente o objetivo. Melhor que encontrei na área para aperfeiçoamento em refatoração.',
+    book_id: '6de9f6b8-5ff4-4e06-b9f4-843eca462803', // Refatoração
+    user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef', // Lindsey Philips
   },
 ]
