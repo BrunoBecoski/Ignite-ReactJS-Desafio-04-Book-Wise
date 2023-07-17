d308f7a974ee91fe4b2d01ed336216b5bed76282
Bruno Becoski <brunobecoski@gmail.com>
2023-07-17 19:32:37 -0300

    Atualizando o componente Date

--- src/components/Date/index.tsx
+++ src/components/Date/index.tsx
@@ -8,7 +8,18 @@ interface DateProps {
 
 export function Date({ date }: DateProps) {
   const dateFormat = dayjs(date).format('DD/MM/YYYY')
+
+  const isToday = dayjs(date).isToday()
+  const isYesterday = dayjs(date).isYesterday()
   const dateToNow = dayjs(date).fromNow()
+
+  if (isToday) {
+    return <Container title={dateFormat}>Hoje</Container>
+  }
+
+  if (isYesterday) {
+    return <Container title={dateFormat}>Ontem</Container>
+  }
 
   return <Container title={dateFormat}>{dateToNow}</Container>
 }
--- src/libs/dayjs.ts
+++ src/libs/dayjs.ts
@@ -1,8 +1,13 @@
 import dayjs from 'dayjs'
 import 'dayjs/locale/pt-br'
+
 import relativeTime from 'dayjs/plugin/relativeTime'
+import isToday from 'dayjs/plugin/isToday'
+import isYesterday from 'dayjs/plugin/isYesterday'
 
 dayjs.locale('pt-br')
 dayjs.extend(relativeTime)
+dayjs.extend(isToday)
+dayjs.extend(isYesterday)
 
 export default dayjs
