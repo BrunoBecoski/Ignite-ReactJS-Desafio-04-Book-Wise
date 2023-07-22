869f733694c4b73044c8123c0996728a1a5bdb97
Bruno Becoski <brunobecoski@gmail.com>
2023-07-21 21:54:50 -0300

    Atualizando o componente SkeletonLoading

--- src/components/SkeletonLoading/styles.ts
+++ src/components/SkeletonLoading/styles.ts
@@ -1,8 +1,29 @@
+import { keyframes } from '@stitches/react'
 import { styled } from '../../styles/stitches.config'
+
+const animation = keyframes({
+  to: { right: '-200%' },
+  from: { right: '200%' },
+})
 
 export const Container = styled('div', {
   borderRadius: '$8',
   background: '$gray-700',
+
+  position: 'relative',
+  overflow: 'hidden',
+
+  '&::after': {
+    position: 'absolute',
+    content: '',
+    width: '50px',
+    height: '100%',
+    transform: 'rotateZ(15deg) scaleY(2)',
+
+    background: 'linear-gradient(90deg, $gray-700, $gray-600, $gray-700)',
+
+    animation: `${animation} 5s infinite linear`,
+  },
 })
 
 export const BookReview = styled(Container, {
